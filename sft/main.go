8e4c10152f74c1823e7983634021f45af2b3e322
MyleneFarmer <jeangamingfg@gmail.com>
2024-01-07 02:17:11 +0100

    Now main.go does a thing
    Added the most basic file transfer capability, with IP & Port, and filepath choice for the client.

--- sft/main.go
+++ sft/main.go
@@ -1,9 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("This program does nothing for now");
+	// Connect to the server
+	reader := bufio.NewReader(os.Stdin)
+
+	// Get the receiver's IP address
+	fmt.Print("Enter receiver's IP address: ")
+	receiverIP, _ := reader.ReadString('\n')
+	receiverIP = strings.TrimSpace(receiverIP) // Remove any trailing newline
+
+	// Get the receiver's port
+	fmt.Print("Enter receiver's Port: ")
+	port, _ := reader.ReadString('\n')
+	port = strings.TrimSpace(receiverIP) // Remove any trailing newline
+
+	// Get the file path
+	fmt.Print("Enter path to the file: ")
+	filepath, _ := reader.ReadString('\n')
+	filepath = strings.TrimSpace(filepath) // Remove any trailing newline
+
+	// Connect to the server
+	conn, err := net.Dial("tcp", receiverIP+":"+port)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	// Open the file to be sent
+	file, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Copy the file to the connection
+	_, err = io.Copy(conn, file)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("This program does nothing for now")
 }
